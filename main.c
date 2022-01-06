8965add2cd05c04ab11334f9885fb82dd3171383
unknown-smtp <94042157+unknown-smtp@users.noreply.github.com>
2022-01-06 17:59:38 +0530

    Biggest Of Three Numbers Using Ternary Operator

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+# include <stdio.h>
+# include <conio.h>
 
+void main()
+{
+    int a, b, c, big ;
+    printf("Enter three numbers : ") ; 
+    scanf("%d %d %d", &a, &b, &c) ;
+    big = a > b ? (a > c ? a : c) : (b > c ? b : c) ;
+    printf("\nThe biggest number is : %d", big) ;
+}
